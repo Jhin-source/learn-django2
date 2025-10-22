1b9655c47d00e0692afa8e177cbe6304fb38c92c
jhinmcglynn@gmail.com <jhinmcglynn@gmail.com>
2025-10-21 23:07:52 -0400

    learn generic viewsets + setup

--- store/serializers.py
+++ store/serializers.py
@@ -10,7 +10,7 @@ class CollectionSerializer(serializers.ModelSerializer):
       'title',
       'products_count'
     ]
-  products_count = serializers.IntegerField()
+  products_count = serializers.IntegerField(read_only=True)
 
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
